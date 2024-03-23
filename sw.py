8747df5970c52c271940a60e2abeedc46b065c5d
misterwo0rldwide <78499936+misterwo0rldwide@users.noreply.github.com>
2024-03-23 22:13:50 +0200

    Add files via upload

--- sw.py
+++ sw.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 from pygame.locals import *
+import math
+import time
 
 
 
@@ -14,6 +16,7 @@ class Server:
 class Game:
     #Frames in second - cycles
     REFRESH_RATE = 60
+    
     
     #Set Clock
     clock = pygame.time.Clock()
@@ -67,41 +70,82 @@ class Game:
 
 
 class Player(Game):
+    
+    GRAVITY_FORCE = 9.81
+
     def __init__(self, speed):
+        super().__init__(1500, 800)
+    
         pygame.joystick.init()
         self.joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
         self.motion = [0, 0]
         self.speed = speed
+        self.inJump = False
         
         self.SetPlayers()
     
     def SetPlayers(self):
-        self.player_image = pygame.transform.scale(pygame.image.load("player.png"), (90,35))
+        self.player_image = pygame.transform.scale(pygame.image.load("player.png"), (70,35))
         self.player_image.set_colorkey((0,0,0))
         self.player_image.convert_alpha()
         self.player_rect = self.player_image.get_rect()
+        self.player_rect.x = 500
+        self.player_rect.y = 850
+    
+    def PlayerJump(self):
+        # we will use one physics equation
+        # y = y0 + v0*t - 5*t**2
+        # x = x0 + v0(t1 - t0)
+        
+        # we will use F=ma to calculate the v0
+        
+
+        if not self.inJump:  # if first time in this function (right after the player pressed jump)
+            self.inJump = True
+            timeDiff = time.time() - self.time
+
+            if timeDiff == 0:
+                timeDiff = 0.1
+            self.Fy = (-(self.player_rect.y - self.yBefore) / timeDiff)*1.3
+            self.Fx = ((self.player_rect.x - self.xBefore) / timeDiff)*1.3
+
+            if self.Fy == 0 and self.Fx == 0:
+                self.inJump = False
+                return
+
+        timeDiff = (time.time() - self.time) * self.speed
+        if self.Fy != 0:
+            self.player_rect.y = self.yBefore - self.Fy * timeDiff + (self.GRAVITY_FORCE / 2) * timeDiff ** 2
+        if self.Fx != 0:
+            self.player_rect.x = self.xBefore + self.Fx * timeDiff
+
+        
+    
     
     def ControllerMovement(self, event):
-        if event.type == JOYBUTTONDOWN:  # jump
-            pass
-            #if event.button == 0:
-            #    my_square_color = (my_square_color + 1) % len(colors)
-        if event.type == JOYAXISMOTION:
+        if event.type == JOYAXISMOTION:  # axis motion
             if event.axis < 2:
                 self.motion[event.axis] = event.value
-        if event.type == JOYDEVICEADDED:
+        if event.type == JOYDEVICEADDED:  # if the device was diconnected
             self.joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
             for joystick in self.joysticks:
                 print(joystick.get_name())
         if event.type == JOYDEVICEREMOVED:
             self.joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                pygame.quit()
-                sys.exit()
+        if event.type == JOYBUTTONDOWN:  # if button was pressed
+            if event.button == 1:  # if O button was pressed
+                self.speed *= 2
+            elif event.button == 0:
+                self.time = time.time()
+                self.PlayerJump()
+                
+        if event.type == JOYBUTTONUP:  # if button was released
+            if event.button == 1:  # if O button was released
+                self.speed /= 2
+        
+        if event.type == pygame.KEYUP:  # the only way to check if the keyboard was released is in event type
+            if event.key == pygame.K_SPACE:
+                self.speed /= 2
                 
         
         if abs(self.motion[0]) < 0.1:
@@ -111,26 +155,44 @@ class Player(Game):
     
     def KeyboardMotion(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_SPACE] and self.speed < 20:
+            self.speed *= 2
+        if keys[pygame.K_LEFT] and self.player_rect.x > 300:
             self.player_rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] and self.player_rect.x < 2100:
             self.player_rect.x += self.speed
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] and self.player_rect.y > 100:
             self.player_rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN] and self.player_rect.y < 850:
             self.player_rect.y += self.speed
     
     def PlayerMotion(self):
+        if not self.inJump:
+            self.xBefore = self.player_rect.x
+            self.yBefore = self.player_rect.y
+
         self.KeyboardMotion()
         
-        self.player_rect.x += self.motion[0] * 5
-        self.player_rect.y += self.motion[1] * 2
+        if self.player_rect.y >= 850 and self.inJump:
+            self.inJump = False
+            self.player_rect.y = 850
+        elif self.inJump:
+            self.PlayerJump()
+        
+        if not self.inJump:
+            self.player_rect.x += self.motion[0] * self.speed
+            if self.player_rect.x > 2100 or self.player_rect.x < 300:
+                self.player_rect.x -= self.motion[0] * self.speed
+            self.player_rect.y += self.motion[1] * 2
+            if self.player_rect.y > 850 or self.player_rect.y < 100:
+                self.player_rect.y -= self.motion[1] * 2
+            
+        
 
 
-game = Game(1500, 800)
 player = Player(7)
-game.GetPlayers(player)
-game.MainLoop()
+player.GetPlayers(player)
+player.MainLoop()
 
 pygame.quit()
 sys.exit()
